c6a2f8744247b0a6d9aa432ca09ddf3c926a6ab2
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-09-19 22:04:36 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dashboard from "./Features/Dashboard";
+import LandingPage from "./Features/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
